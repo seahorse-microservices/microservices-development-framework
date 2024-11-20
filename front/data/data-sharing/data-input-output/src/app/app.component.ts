3027a78b4a6fbee1410821455a555ccddec88866
Jaime Gómez <jaimegomez@Jaimes-MacBook-Pro.local>
2024-11-20 17:11:30 +0100

    angular sharing data between components using input and outpu decorators

---
+++ front/data/data-sharing/data-input-output/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from './parent.component';
+import { ChildComponent } from './child.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,  ParentComponent, ChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'data-input';
+}
--- networking/seahorse-sync-restful-api/sync-restful-api-backend/dist/books/book.entity.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Book {
-    id: number;
-    title: string;
-    author: string;
-}
