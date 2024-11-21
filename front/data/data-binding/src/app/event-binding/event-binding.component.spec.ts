2e81bbf70bda2bbae9148d06e03d5e8d2230f600
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-11-21 14:59:20 +0000

    Only event binding is left

--- front/data/data-binding/src/app/event-binding/event-binding.component.spec.ts
+++ front/data/data-binding/src/app/event-binding/event-binding.component.spec.ts
@@ -1,22 +1,13 @@
-import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
 import { EventBindingComponent } from "./event-binding.component";
+import { render, screen } from '@testing-library/angular';
+
 
 describe("EventBindingComponent", () => {
-  let fixture: ComponentFixture<EventBindingComponent>;
-  let eventBindingComp: EventBindingComponent;
   
-  beforeEach(waitForAsync(() => {
-		TestBed.configureTestingModule({imports: [EventBindingComponent]}).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EventBindingComponent);
-    eventBindingComp = fixture.componentInstance;
-		fixture.detectChanges();
-  });
-
-  it("should create", () => {
-    expect(eventBindingComp).toBeDefined();
+  beforeEach(async() => {
+		await render(EventBindingComponent, {
+			
+		})
   });
 
   it("clicking on the buttons should change the component's clickCount property in their way", () => {
--- front/data/data-binding/src/app/two-way-binding/two-way-binding.component.spec.ts
+++ front/data/data-binding/src/app/two-way-binding/two-way-binding.component.spec.ts
@@ -1,8 +1,5 @@
-import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
 import { TwoWayBindingComponent } from "./two-way-binding.component";
-import { SizerComponent } from "./sizer/sizer.component";
-import { fireEvent, render, screen } from '@testing-library/angular';
-import { By } from "@angular/platform-browser";
+import { render, screen } from '@testing-library/angular';
 
 describe("TwoWayBindingComponent", () => {
   
@@ -16,20 +13,15 @@ describe("TwoWayBindingComponent", () => {
   });
 
 	it("the font size should be two-way binded", () => {
-		const sizersResizableParagraph = screen.getByText("FontSize: 16px");
-		const decrementFontSizeButton = screen.getByRole("button", {name: "+"});
-		const incrementFontSizeButton = screen.getByRole("button", {name: "-"});
+		expect(screen.getByText("FontSize: 16px")).toBeTruthy();
 		
-		expect(sizerComp.size)
-		.withContext("initial sizerComp's size compared to twoWayBindingComp's one")
-		.toBe(twoWayBindingComp.fontSizePx);
-		btnDecrement.click();
-		expect(twoWayBindingComp.fontSizePx)
-		.withContext("expected twoWayBindingComp's fontSizePx after sizerComp's dec()")
-		.toBe(Number(sizerComp.size));
-		btnIncrement.click();
-		expect(twoWayBindingComp.fontSizePx)
-		.withContext("expected twoWayBindingComp's fontSizePx after sizerComp's inc()")
-		.toBe(Number(sizerComp.size));
+		const decrementFontSizeButton = screen.getByRole("button", {name: "-"});
+		const incrementFontSizeButton = screen.getByRole("button", {name: "+"});
+		
+		decrementFontSizeButton.click();
+		expect(screen.getByText('Resizable Text').style.fontSize).toBe("15px");
+
+		incrementFontSizeButton.click();
+		expect(screen.getByText('Resizable Text').style.fontSize).toBe("16px");
 	});
 });
