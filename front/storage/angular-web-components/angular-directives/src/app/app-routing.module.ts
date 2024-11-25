c3e4093085e4c06cef77ebf9a5148796a15110c8
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-11-25 13:05:14 +0000

    If directive code done

--- front/storage/angular-web-components/angular-directives/src/app/app-routing.module.ts
+++ front/storage/angular-web-components/angular-directives/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgclassAttributeComponent } from './ngclass-attribute/ngclass-attribute.component';
 import { NgstyleAttributeComponent } from './ngstyle-attribute/ngstyle-attribute.component';
 import { NgmodelAttributeComponent } from './ngmodel-attribute/ngmodel-attribute.component';
+import { IfDirectiveComponent } from './if-directive/if-directive.component';
 
 const routes: Routes = [
+	{ path: 'if-directive', component: IfDirectiveComponent },
   { path: 'ngclass-directive', component: NgclassAttributeComponent },
 	{ path: 'ngstyle-directive', component: NgstyleAttributeComponent },
 	{ path: 'ngmodel-directive', component: NgmodelAttributeComponent },
