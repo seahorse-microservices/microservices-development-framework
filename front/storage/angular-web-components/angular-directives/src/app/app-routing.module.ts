08087bd10c344e316c88cd0bcf66e36f12417450
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-11-25 12:47:36 +0000

    NgModel, NgClass, NgStyle

---
+++ front/storage/angular-web-components/angular-directives/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgclassAttributeComponent } from './ngclass-attribute/ngclass-attribute.component';
+import { NgstyleAttributeComponent } from './ngstyle-attribute/ngstyle-attribute.component';
+import { NgmodelAttributeComponent } from './ngmodel-attribute/ngmodel-attribute.component';
+
+const routes: Routes = [
+  { path: 'ngclass-directive', component: NgclassAttributeComponent },
+	{ path: 'ngstyle-directive', component: NgstyleAttributeComponent },
+	{ path: 'ngmodel-directive', component: NgmodelAttributeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
