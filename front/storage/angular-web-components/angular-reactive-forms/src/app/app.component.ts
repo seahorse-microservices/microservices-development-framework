6cd45a8a7f95879e855ece8e552f42b9b3be0850
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-11-26 08:59:47 +0000

    Angular Reactive Forms code done

--- front/storage/angular-web-components/angular-reactive-forms/src/app/app.component.ts
+++ front/storage/angular-web-components/angular-reactive-forms/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {  FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 class FormValidation {
   static match(controlName: string, checkControlName: string): ValidatorFn {
@@ -24,110 +23,7 @@ class FormValidation {
 
 @Component({
   selector: 'app-root',
-  template: ` <div class="register-form">
-  <form [formGroup]="form" (ngSubmit)="onSubmit()">
-    <div class="form-group">
-      <label>Full Name</label>
-      <input
-        type="text"
-        formControlName="fullname"
-        class="form-control"
-        [ngClass]="{ 'is-invalid': submitted && f['fullname'].errors }"
-      />
-      @if (submitted && f['fullname'].errors) {
-      <div class="invalid-feedback">
-        @if (f['fullname'].errors['required']) {
-        <div>Fullname is required</div>
-        }
-      </div>
-      }
-    </div>
-
-    <div class="form-group">
-      <label>Username</label>
-      <input
-        type="text"
-        formControlName="username"
-        class="form-control"
-        [ngClass]="{ 'is-invalid': submitted && f['username'].errors }"
-      />
-      @if (submitted && f['username'].errors) {
-      <div class="invalid-feedback">
-        @if (f['username'].errors['required']) {
-        <div>Username is required</div>
-        }
-      </div>
-      }
-    </div>
-
-    <div class="form-group">
-      <label>Email</label>
-      <input
-        type="text"
-        formControlName="email"
-        class="form-control"
-        [ngClass]="{ 'is-invalid': submitted && f['email'].errors }"
-      />
-      @if (submitted && f['email'].errors) {
-      <div class="invalid-feedback">
-        @if (f['email'].errors['required']) {
-        <div>Email is required</div>
-        }
-		@if (f['email'].errors['email']) {
-        <div>Email is invalid</div>
-        }
-      </div>
-      }
-    </div>
-
-    <div class="form-group">
-      <label>Password</label>
-      <input
-        type="password"
-        formControlName="password"
-        class="form-control"
-        [ngClass]="{ 'is-invalid': submitted && f['password'].errors }"
-      />
-      @if (submitted && f['password'].errors) {
-      <div class="invalid-feedback">
-        @if (f['password'].errors['required']) {
-        <div>Password is required</div>
-        }
-      </div>
-      }
-    </div>
-
-    <div class="form-group">
-      <label>Confirm Password</label>
-      <input
-        type="password"
-        formControlName="confirmPassword"
-        class="form-control"
-        [ngClass]="{ 'is-invalid': submitted && f['confirmPassword'].errors }"
-      />
-      @if (submitted && f['confirmPassword'].errors) {
-      <div class="invalid-feedback">
-        @if (f['confirmPassword'].errors['required']) {
-        <div>Confirm Password is required</div>
-        }
-		@if (f['confirmPassword'].errors['matching']) {
-        <div>Confirm Password does not match</div>
-        }
-      </div>
-      }
-    </div>
-
-    <div class="form-group">
-      <button type="submit" class="btn btn-primary">Register</button>
-    </div>
-  </form>
-</div> `
-,
-styles: [
-  
-]
-  // templateUrl: './app.component.html',
-  // styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   form: FormGroup;
@@ -153,11 +49,11 @@ export class AppComponent {
   }
 
   onSubmit(): void {
-  //   this.submitted = true;
-  //   if (this.form.invalid) {
-  //     return;
-  //   }
-  //  console.log(JSON.stringify(this.form.value, null, 2));
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+   console.log(JSON.stringify(this.form.value, null, 2));
   }
 
   onReset(): void {
--- front/storage/angular-web-components/angular-reactive-forms/src/app/app.module.ts
+++ front/storage/angular-web-components/angular-reactive-forms/src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgClass } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-		RouterOutlet, 
-		ReactiveFormsModule, 
-		NgClass
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
